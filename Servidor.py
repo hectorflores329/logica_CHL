64f77f65e662f322b6566117e1495f5da694c70e
hectorflores329 <hectorflores329@gmail.com>
2021-05-05 11:59:28 -0600

    Commit

--- Servidor.py
+++ Servidor.py
@@ -20,8 +20,7 @@ def DescargarCSV():
     df = pd.read_csv("https://github.com/MinCiencia/Datos-COVID19/raw/master/output/producto3/TotalesPorRegion.csv")
     df.to_csv("archivo.csv")
 
+    return
 
 if __name__ == '__main__':
-    print('Empezando proceso de descarga.')
     DescargarCSV()
-    print('El roceso de descarga ha finalizado.')
